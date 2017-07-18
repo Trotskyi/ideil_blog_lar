639d4bbef141cc74e40b6f6b90df713700d75462
Trotskyi Oleksandr <trotskyioleksandr@gmail.com>
2017-07-18 13:50:09 +0300

    Create databases

---
+++ database/seeds/PostsTagsTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Ideal\PostTag;
+use Ideal\Post;
+use Ideal\Tag;
+
+class PostsTagsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+    }
+}
