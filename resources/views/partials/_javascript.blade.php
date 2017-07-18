9890541a65d30e3847ef759513f56c89ae8e3d42
Trotskyi Oleksandr <trotskyioleksandr@gmail.com>
2017-07-18 14:14:03 +0300

    resource view

---
+++ resources/views/partials/_javascript.blade.php
@@ -0,0 +1,12 @@
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+<script src="{{ asset('/js/modernizr.js') }}"></script>
+{{ HTML::script('js/modernizr.js', array('media' => 'print')) }}
+
+<script src="{{ asset('/js/jquery-1.10.2.min.js') }}"></script>
+
+
+<script src="{{ asset('/js/index.js') }}"></script>
