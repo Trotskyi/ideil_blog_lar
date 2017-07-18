da837bbf7d4872efca4f9462aa15a6e97322bf9c
Trotskyi Oleksandr <trotskyioleksandr@gmail.com>
2017-07-18 14:26:27 +0300

    modf routes

--- routes/web.php
+++ routes/web.php
@@ -51,4 +51,4 @@ Route::group(['middleware' => ['web']], function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
