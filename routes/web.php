56f19ebd95efd1a91449a3129b29195c6001f353
Trotskyi Oleksandr <trotskyioleksandr@gmail.com>
2017-07-18 14:16:56 +0300

    modf routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,44 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => ['web']], function () {
+    // Authentication Routes
+    Route::get('auth/login', ['as' => 'login', 'uses' => 'Auth\LoginController@getLogin']);
+    Route::post('auth/login', 'Auth\LoginController@postLogin');
+    Route::get('auth/logout', ['as' => 'logout', 'uses' => 'Auth\LoginController@getLogout']);
+
+    // Registration Routes
+    Route::get('auth/register', 'Auth\AuthController@getRegister');
+    Route::post('auth/register', 'Auth\AuthController@postRegister');
+
+    // Password Reset Routes
+    Route::get('password/reset/{token?}', 'Auth\PasswordController@showResetForm');
+    Route::post('password/email', 'Auth\PasswordController@sendResetLinkEmail');
+    Route::post('password/reset', 'Auth\PasswordController@reset');
+
+    // Categories
+    Route::resource('categories', 'CategoryController', ['except' => ['create']]);
+    Route::delete('categories/{id}', ['uses' => 'CategoryController@destroy', 'as' => 'categories.destroy']);
+    Route::resource('tags', 'TagController', ['except' => ['create']]);
+
+    // Comments
+    Route::post('comments/{post_id}', ['uses' => 'CommentsController@store', 'as' => 'comments.store']);
+    Route::get('comments/{id}/edit', ['uses' => 'CommentsController@edit', 'as' => 'comments.edit']);
+    Route::put('comments/{id}', ['uses' => 'CommentsController@update', 'as' => 'comments.update']);
+    Route::delete('comments/{id}', ['uses' => 'CommentsController@destroy', 'as' => 'comments.destroy']);
+    Route::get('comments/{id}/delete', ['uses' => 'CommentsController@delete', 'as' => 'comments.delete']);
+
+    //Main page
+    Route::get('blog/{slug}', ['as' => 'blog.single', 'uses' => 'BlogController@getSingle'])->where('slug', '[\w\d\-\_]+');
+    Route::get('blog', ['uses' => 'BlogController@getIndex', 'as' => 'blog.index']);
+    Route::get('contact', 'PagesController@getContact');
+    Route::post('contact', 'PagesController@postContact');
+    Route::get('about', 'PagesController@getAbout');
+    Route::get('/', 'PagesController@getIndex');
+    Route::resource('posts', 'PostController');
 });
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
